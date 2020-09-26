9df89394dbc5a424978aa75c518d99e3f9271763
Ukjeong Lee <nearfri@gmail.com>
2020-09-26 11:09:36 +0900

    샘플앱 테스트 코드 추가

---
+++ Projects/ResourceKeyApp/ResourceKeyAppTests/ResourceKey/ColorKeyTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import ResourceKeyApp
+
+class ColorKeyTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        for key in ColorKey.allGeneratedKeys {
+            XCTAssertNotNil(UIColor(named: key.rawValue, in: .module, compatibleWith: nil),
+                            "\(key.rawValue) color not found")
+        }
+    }
+    
+}
---
+++ Projects/ResourceKeyApp/ResourceKeyAppTests/ResourceKey/ImageKeyTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import ResourceKeyApp
+
+class ImageKeyTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        for key in ImageKey.allGeneratedKeys {
+            XCTAssertNotNil(UIImage(named: key.rawValue, in: .module, compatibleWith: nil),
+                            "\(key.rawValue) image not found")
+        }
+    }
+    
+}
