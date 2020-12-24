e7317cfc2041b9fba43388e448dcc7c442f72df6
Ukjeong Lee <nearfri@gmail.com>
2020-12-24 23:33:51 +0900

    Fix build scripts of ResourceKeyApp

--- Projects/ResourceKeyApp/Tools/run-generate-asset-keys.sh
+++ Projects/ResourceKeyApp/Tools/run-generate-asset-keys.sh
@@ -3,7 +3,7 @@
 # 에러가 발생하면 스크립트 종료
 set -e
 
-swift run --package-path ../../../ generate-asset-keys \
+swift run --package-path ../../../ resourcekey generate-asset-keys \
     --input-xcassets ../ResourceKeyApp/Assets.xcassets \
     --asset-type image \
     --key-type-name ImageKey \
@@ -11,7 +11,7 @@ swift run --package-path ../../../ generate-asset-keys \
     --key-decl-file ../ResourceKeyApp/ResourceKey/ImageKey.swift \
     --key-list-file ../ResourceKeyAppTests/ResourceKey/AllImageKeys.swift
 
-swift run --package-path ../../../ generate-asset-keys \
+swift run --package-path ../../../ resourcekey generate-asset-keys \
     --input-xcassets ../ResourceKeyApp/Assets.xcassets \
     --asset-type color \
     --key-type-name ColorKey \
--- Projects/ResourceKeyApp/Tools/run-generate-localizable-strings.sh
+++ Projects/ResourceKeyApp/Tools/run-generate-localizable-strings.sh
@@ -3,7 +3,7 @@
 # 에러가 발생하면 스크립트 종료
 set -e
 
-swift run --package-path ../../../ generate-localizable-strings \
+swift run --package-path ../../../ resourcekey generate-localizable-strings \
     --input-source ../ResourceKeyApp/ResourceKey/StringKey.swift \
     --resources ../ResourceKeyApp \
     --value-strategy ko:comment \
