94d2989fe1e8f58cc6a2bc7dee3d1b7f5701d0c4
Ukjeong Lee <nearfri@gmail.com>
2021-01-04 22:44:10 +0900

    Add "\r" character handling to script

--- Projects/XCResourceApp/Tools/run-csv2strings.sh
+++ Projects/XCResourceApp/Tools/run-csv2strings.sh
@@ -3,6 +3,11 @@
 # 에러가 발생하면 스크립트 종료
 set -e
 
+CSV_PATH="./SampleApp-translation.csv"
+
+# "\r" 문자를 지운다.
+grep -q '\r' $CSV_PATH && sed -i '' 's/\r//g' $CSV_PATH
+
 swift run --package-path ../../../ xcresource csv2strings \
-    --csv-path ./SampleApp-translation.csv \
+    --csv-path $CSV_PATH \
     --resources-path ../XCResourceApp
