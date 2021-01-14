c7a8246dcfa3c565d15c04724f8a458edfcf0ae0
Ukjeong Lee <nearfri@gmail.com>
2021-01-14 11:58:29 +0900

    Update sample scripts

--- Projects/XCResourceApp/Tools/run-swift2strings.sh
+++ Projects/XCResourceApp/Tools/run-swift2strings.sh
@@ -3,7 +3,7 @@
 # 에러가 발생하면 스크립트 종료
 set -e
 
-swift run --package-path ../../../ xcresource swift2strings \
+xcrun --sdk macosx swift run --package-path ../../../ xcresource swift2strings \
     --swift-path ../XCResourceApp/ResourceKeys/StringKey.swift \
     --resources-path ../XCResourceApp \
     --language ko \
--- Projects/XCResourceApp/Tools/run-xcassets2swift.sh
+++ Projects/XCResourceApp/Tools/run-xcassets2swift.sh
@@ -3,13 +3,13 @@
 # 에러가 발생하면 스크립트 종료
 set -e
 
-swift run --package-path ../../../ xcresource xcassets2swift \
+xcrun --sdk macosx swift run --package-path ../../../ xcresource xcassets2swift \
     --xcassets-path ../XCResourceApp/Assets.xcassets \
     --asset-type image \
     --swift-path ../XCResourceApp/ResourceKeys/ImageKey.swift \
     --swift-type-name ImageKey
 
-swift run --package-path ../../../ xcresource xcassets2swift \
+xcrun --sdk macosx swift run --package-path ../../../ xcresource xcassets2swift \
     --xcassets-path ../XCResourceApp/Assets.xcassets \
     --asset-type color \
     --swift-path ../XCResourceApp/ResourceKeys/ColorKey.swift \
