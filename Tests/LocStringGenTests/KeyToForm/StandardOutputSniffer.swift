5ed9730e55500f47665e46a6442a888063633af9
Ukjeong Lee <nearfri@gmail.com>
2021-12-03 23:57:44 +0900

    Revert "Remove code waiting to close file handle"
    This reverts commit eff2c585118c67591f85e099d7af32862dcaf84b.

--- Tests/LocStringGenTests/KeyToForm/StandardOutputSniffer.swift
+++ Tests/LocStringGenTests/KeyToForm/StandardOutputSniffer.swift
@@ -37,6 +37,8 @@ class StandardOutputSniffer {
     private func synchronize() {
         try? replacementPipe.fileHandleForWriting.synchronize()
         try? replacementPipe.fileHandleForReading.synchronize()
+        
+        RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.005))
     }
     
     private func rollback() {
