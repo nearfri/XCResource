a3cd8d9e8cf37efaa70aa716b9c92231a09d1a2e
Ukjeong Lee <nearfri@gmail.com>
2022-11-13 10:22:12 +0900

    Update to 0.9.20

--- Sources/XCResourceCommand/Commands/XCResource.swift
+++ Sources/XCResourceCommand/Commands/XCResource.swift
@@ -5,7 +5,7 @@ public struct XCResource: ParsableCommand {
     public static let configuration: CommandConfiguration = .init(
         commandName: "xcresource",
         abstract: "리소스 코드 생성 유틸리티",
-        version: "0.9.19",
+        version: "0.9.20",
         subcommands: [
             InitManifest.self, RunManifest.self,
             XCAssetsToSwift.self, KeyToForm.self,
