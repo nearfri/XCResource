c898c7997a6699f7fc6dc5420bbec4c61e700f39
Ukjeong Lee <nearfri@gmail.com>
2025-05-29 14:24:01 +0900

    Update to 1.1.2

--- Examples/XCResourceExample/XCResourceExampleLib/Package.swift
+++ Examples/XCResourceExample/XCResourceExampleLib/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../../../"),
-//        .package(url: "https://github.com/nearfri/XCResource-plugin.git", from: "1.1.1"),
+//        .package(url: "https://github.com/nearfri/XCResource-plugin.git", from: "1.1.2"),
     ],
     targets: [
         // MARK: - Core Modules
